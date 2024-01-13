1aa45d9356c7223814376ad377b6f8ce5187b410
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-13 13:32:35 +0530

    deleted/ clear repository of development branch

--- demo/middlename.js
+++
@@ -1,1 +0,0 @@
-alert("middle name is subhahs   ");
--- demo/name.js
+++
@@ -1,4 +0,0 @@
-console.log("my name is arjun")
-<script  typeo="text/javascript">
-    document.write("Java script is simpple language for self learner");
-    </scrpt>
