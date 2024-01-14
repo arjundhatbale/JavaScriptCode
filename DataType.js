a6cd4ba05380d2caa581969e0942ed10189568ab
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-14 09:55:41 +0530

    Java Script Basics

---
+++ DataType.js
@@ -0,0 +1,119 @@
+// //  Primitive data type
+
+// let age = 49;
+// let price = 299.23;
+// let name = "arjun";
+// let x;
+// let a = true;
+// let b = BigInt("23");
+
+
+// // Objects
+
+// // It is a collectin of values. 
+
+// const student  = {
+//     fullName : "Rahul Kumar",
+//     age : 20,
+//     cgpa : 9.2,
+//     isPass : true
+// };
+
+// console.log(student);
+// console.log(student.age + 1);
+// console.log(student["isPass"]);
+
+
+
+// Object 
+
+// const  product ={
+//     title : "Ball Pen",
+//     rating : 2,
+//     offer : 5,
+//     price : 270
+// };
+
+// console.log(product);
+
+
+
+// const profile = {
+//     profileName : "arjundhatble",
+//     deatail : "This is my Instagram Account",
+//     followers  : 234,
+//     isFollow : false,
+//     follwing : 85
+
+// };
+// console.log(typeof profile["isFollow"]);
+
+
+
+// Conditional statements 
+
+let age = 13; 
+
+if(age > 5){
+    return true;
+}else{
+    return false
+};
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ first.js
@@ -0,0 +1,55 @@
+// console.log("arjun");
+// console.log("dhatbale  ");
+
+// fullName = "Arjun dhatbale";
+// console.log("fullName " + fullName);
+// age = 24; 
+// price = 23.32;
+// x = null ; 
+// y = undefined; 
+// console.log(y, x , age, " ", " + " , price);
+// isfollow = true;
+// console.log(!isfollow);
+
+// fullName = 222;
+// console.log( "fullName " + fullName);
+
+// fullname = "arjun";
+// fullName = "Arjun";
+// console.log(fullname);
+// console.log(fullName);
+
+var age= 23;
+var age = 24; 
+var age = 44; 
+console.log(age);
+let name = "arjun";
+  name = "dhatbale";
+  
+ console.log(name);
+
+
+ {
+    let a = 20; 
+    console.log("a = ", a);
+ }
+ {
+    let a = 20; 
+    console.log("aa = " + a);
+ }
+ {
+    const a = 20; 
+    console.log("a = ", a);
+ }
+ {
+    const a = 20; 
+    console.log("aa = " + a);
+ }
+ {
+    var a = 20; 
+    console.log("a = ", a);
+ }
+ {
+    var a = 20; 
+    console.log("aa = " + a);
+ }
