e5968148bf07cc25196be5c3ad0d8ceb3544e547
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-14 19:08:14 +0530

    learned till functions

---
+++ Functions.js
@@ -0,0 +1,87 @@
+// funciton in javascript
+
+
+// functionfunctionName(){
+//     // so some work / tasek /logic
+// }
+
+// funciton functonName (paramer 1, paramete 2){
+
+// }
+// let msg = "my name is arjun ";
+
+// function myFunction(msg){
+//     console.log("This is my first funciton");
+//     console.log(msg);
+//     msg;
+
+// }
+
+// console.log(myFunction);
+// myFunction(msg);
+
+// functio -. 2 number , sum 
+
+// function sum(n1, n2){
+//     console.log(n1 + n2);
+// }
+// sum(1,5);
+
+// function sum (x,y){
+//     s = x + y; 
+//     return s; 
+// }
+//  let val = sum(3,5);
+// console.log ("value = " ,val);
+
+// function mul(a,b){
+//     return a*b;
+// }
+// mul(2,2);
+// console.log(mul(2,4));
+
+// const arrowSum = (a,b) => {
+//     console.log(a + b);
+// }
+
+// forEach Loop in Arrays 
+
+ 
+let arr = ["a","b","c","Mumbai","Delhi"];
+// arr.forEach(function printVal(val){ // value at each index
+//     console.log(val);
+
+// });
+
+//  arr.forEach((val)=> {
+//     console.log(val);
+//  });
+// arr.forEach((val, idx, arr) => {
+//    console.log(val.toUpperCase(),idx,arr);
+// });
+
+// ***************
+
+
+let num = [1,2,3,5,6,7];
+// num.forEach((num  ) => {
+//    console.log(num * num);
+// });
+let newArr = num.map((val) => {
+   return val;
+});
+console.log(newArr);
+// num.map((val) => {
+//    console.log(val);
+// });
+
+
+
+
+
+
+
+
+
+
+
