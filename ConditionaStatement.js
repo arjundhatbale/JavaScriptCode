b5a957591ca391684026da61f56aa7425a86d39e
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-14 10:18:57 +0530

    Notification

---
+++ ConditionaStatement.js
@@ -0,0 +1,35 @@
+// Conditional statements
+
+let age = 2; 
+if(age > 18){
+    console.log("Can vote");
+}else{
+    console.log("Can not vote");
+};
+
+let mode = "silver";
+let color; 
+if(mode === "dark"){
+    color = "black";
+} else if (mode == "blue"){
+    color = "blue";
+}else{
+    color = " color not found";
+}
+ 
+console.log(color);
+
+//  odd or even 
+
+let num = 2; 
+if(num % 2 == 0){
+    console.log("even");
+}else if(num % 2 != 0){
+    console.log("odd");
+}
+
+// alert("Hello");
+
+//prompt("hello Arjun");
+
+let name = prompt("hello");
