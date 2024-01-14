37534c20b0a5b7cec2d7e4d03dbff2dfb43af993
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-14 19:08:14 +0530

    Updated

---
+++ Functions.js
@@ -0,0 +1,76 @@
+// funciton in javascript
+
+
+// functionfunctionName(){
+//     // so some work / tasek /logic
+// }
+
+// funciton functonName (paramer 1, paramete 2){
+
+// }
+// let msg = "my name is arjun ";
+
+// function myFunction(msg){
+//     console.log("This is my first funciton");
+//     console.log(msg);
+//     msg;
+
+// }
+
+// console.log(myFunction);
+// myFunction(msg);
+
+// functio -. 2 number , sum 
+
+// function sum(n1, n2){
+//     console.log(n1 + n2);
+// }
+// sum(1,5);
+
+// function sum (x,y){
+//     s = x + y; 
+//     return s; 
+// }
+//  let val = sum(3,5);
+// console.log ("value = " ,val);
+
+// function mul(a,b){
+//     return a*b;
+// }
+// mul(2,2);
+// console.log(mul(2,4));
+
+// const arrowSum = (a,b) => {
+//     console.log(a + b);
+// }
+
+// forEach Loop in Arrays 
+
+ 
+let arr = ["a","b","c","Mumbai","Delhi"];
+// arr.forEach(function printVal(val){ // value at each index
+//     console.log(val);
+
+// });
+
+ arr.forEach((val)=> {
+    console.log(val);
+ });
+arr.forEach((val, idx, arr) => {
+   console.log(val.toUpperCase(),idx,arr);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- string.js
+++ string.js
@@ -1,5 +1,21 @@
 // string
 
-let str1 = "ApnaCollege";
-let str2 = "Arjun";
-console.log(str1[2]);
+// let str1 = "ApnaCollege";
+// let str2 = "Arjun";
+// console.log(str1[2]);
+
+
+// // Template Literals
+
+// let sentence = `This is a Template Literal`; 
+// console.log(sentence);
+
+
+let obj = {
+    item : "pen",
+    price : 10,
+};
+console.log(  `thecost \n of ${obj.item} is ${obj.price} ruppees`);
+let output = `thecost of ${obj.item} is ${obj.price} ruppees`;
+console.log(output);
+console.log("the cost of ", obj.item , "is ",  obj.price);
