715845c5888d4ee6ab074f1dbd3f9311bade0775
arjundhatbale <arjundhatbale321@gmail.com>
2024-01-14 10:58:25 +0530

    loops

---
+++ loop.js
@@ -0,0 +1,82 @@
+// console.log("This is loop program file");
+
+// let a = 5; 
+// for(let  i = 1; i <= 5; i++){
+//     console.log("hi");
+// };
+
+// let sum =0; 
+// let n = 2; 
+// for (let i = 0;  i < 5; i ++ ){
+//     sum =  sum + n; 
+//  }
+//  console.log("sum = ", sum ); 
+
+
+// for(var i = 1 ; i <= 5; i ++){
+//     console.log("i = ", i);
+// }
+// console.log("final i = ", i);
+// let j = 1; 
+// do{
+//     console.log("j = ", j)
+//     j ++ ;
+// }while(j < 10);
+
+//////////////////////
+
+// for Of loop   only of string and Array
+
+
+// let str = "ApanaCollege";
+
+// for(let i of str){ // single iterator 
+//     console.log("i = ",i);
+// };
+
+// for In loop   use of Objects
+
+
+let student ={
+    name : "arjun dhatbale",
+    age : 24,
+    weight : 54,
+    isPass : true,
+};
+
+for(let s in student){
+    console.log("student = ", student);
+    console.log("key = ", s, " value = ", student[s]);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
